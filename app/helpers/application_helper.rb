4f122e86527364b1dabed0e9478ee6152d28e491
ataruby <atarubift@gmail.com>
2020-10-17 15:15:31 +0900

    add application_helper full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # ページごとの完全なタイトルを返す
+    def full_title(page_title = '')
+        base = "Todo App"
+        if page_title.empty?
+            base
+        else
+            page_title + " | " + base
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root 'basic_pages#home'
   get  'help' => 'basic_pages#help'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
