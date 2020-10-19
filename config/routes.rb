903ec7b3fa25a09d91d6ce7c9cf7d5240a50e4de
ataruby <atarubift@gmail.com>
2020-10-19 11:35:52 +0900

    add login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'basic_pages#home'
   get  'help' => 'basic_pages#help'
   get  'signup' => 'users#new'
+  get  'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get  'logout' => 'sessions#destroy'
   resources :users
 end
