2c00c6f25c073a836d23dd6b9c6d6996f4e07140
ataruby <atarubift@gmail.com>
2020-10-19 13:32:03 +0900

    add remember func

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,28 @@ class User < ApplicationRecord
     uniqueness: true
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
+
+    # 文字列のハッシュ値を返す
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost? BCrypt::Engine::MIN_COST :
+                                                     BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    # ランダムなトークンを返す
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # データベースに保存
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # 渡されたトークンがダイジェストと一致したらtrueを返す
+    def authenticated?(remember_token)
+        BCrypt::Password.new(remember_digest).is_passward?(remember_token)
+    end
 end
 
