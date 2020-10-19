74b8a6be504000a6c864bac7eb1d0cf50f097992
ataruby <atarubift@gmail.com>
2020-10-19 13:04:21 +0900

    add logout func

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # @user.send_activation_email
+      log_in @user
       flash[:info] = "アカウントを有効にするには、メールをご確認ください。"
       redirect_to root_url
     else
